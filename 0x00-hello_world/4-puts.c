cdb35c324750e142bff550f0df917392fcc2e0fc
Akinsanya Farouk Morakinyo <akinsanyafm@gmail.com>
2022-07-02 20:22:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * to start our program
+  * Prints a string to output
+  * Return: 0 is success
+  */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
